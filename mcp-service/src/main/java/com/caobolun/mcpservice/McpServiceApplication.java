5d6b5d51ce5e8a8d8122bfb4f53d9cdd09404bfa
cbl <cao_bolun@qq.com>
2026-08-04 16:20:15 +0800

    新增MCP

---
+++ mcp-service/src/main/java/com/caobolun/mcpservice/McpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.caobolun.mcpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServiceApplication.class, args);
+    }
+
+}
